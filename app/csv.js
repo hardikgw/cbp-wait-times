7f2422b92592016e510817f7ad0c3a5e2c48dd99
Hardik Patel <hardikgw@gmail.com>
2016-11-27 08:27:39 -0500

    minor

--- app/csv.js
+++ app/csv.js
@@ -8,7 +8,19 @@ let FileWriter = function (filename) {
 };
 FileWriter.prototype.write = function (data) {
     if (!this.headerWritten) {
-        this.headers = Object.keys(data);
+        this.headers = Object.keys(data).sort(function(a, b) {
+            let nameA = a.toUpperCase();
+            let nameB = b.toUpperCase();
+            if (nameA < nameB) {
+                return -1;
+            }
+            if (nameA > nameB) {
+                return 1;
+            }
+
+            // names must be equal
+            return 0;
+        });
         fs.writeFileSync(this.filename, this.headers.concat() + "\n");
         this.headerWritten = true;
     }
